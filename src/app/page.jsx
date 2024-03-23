5e7aead723b92e60741126868ee7da61db2f3eb8
abdulkareemm <eng.kreem.96@gmail.com>
2024-03-23 20:12:23 +0300

    fix link

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 "use client"
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const Homepage = () => {
   return (
@@ -30,12 +31,18 @@ const Homepage = () => {
           </p>
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
-            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+            <Link
+              href={"/portfolio"}
+              className="p-4 rounded-lg ring-1 ring-black bg-black text-white"
+            >
               View My Work
-            </button>
-            <button className="p-4 rounded-lg ring-1 ring-black">
+            </Link>
+            <Link
+              href={"/contact"}
+              className="p-4 rounded-lg ring-1 ring-black"
+            >
               Contact Me
-            </button>
+            </Link>
           </div>
         </div>
       </div>
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -48,14 +48,14 @@ export default function Portfolio() {
     <motion.div
       className="h-full"
       initial={{ y: "-200vh" }}
-      animate={{ y: "0%" }} 
+      animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
       <div className="h-[1000vh] relative" ref={ref}>
         <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
           My Works
         </div>
-        <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden ">
+        <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
           <motion.div style={{ x }} className="flex">
             <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-purple-300 to-red-300" />
             {items.map((item) => (
